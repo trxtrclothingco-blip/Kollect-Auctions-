055fdb894be9123ca3c326fd895a274089feabd4
trxtrclothingco-blip <trxtrclothingco@gmail.com>
2026-01-25 18:25:09 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCGfejcFwdLTKow_yPptWfbdyrcc2b6dNc",
   authDomain: "kollectauctions-b9de9.firebaseapp.com",
   projectId: "kollectauctions-b9de9",
-  storageBucket: "kollectauctions-b9de9.firebasestorage.app",
+  storageBucket: "kollectauctions-b9de9.appspot.com",
   messagingSenderId: "185844480439",
-  appId: "1:185844480439:web:36de485ff58fcca2cc5032",
-  measurementId: "G-DJ0NLNG2LN"
+  appId: "1:185844480439:web:36de485ff58fcca2cc5032"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-console.log("Firebase connected:", app.name);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
