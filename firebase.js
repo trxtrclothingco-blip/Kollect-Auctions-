76826bb06a1b15e05bd640a962b346c95f083212
trxtrclothingco-blip <trxtrclothingco@gmail.com>
2026-01-26 15:39:54 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,10 @@
 // firebase.js
-// --------------------
-// Modular Firebase Setup
-// --------------------
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
-// Your Firebase configuration
+// Firebase config
 export const firebaseConfig = {
   apiKey: "AIzaSyCGfejcFwdLTKow_yPptWfbdyrcc2b6dNc",
   authDomain: "kollectauctions-b9de9.firebaseapp.com",
@@ -19,10 +15,8 @@ export const firebaseConfig = {
   measurementId: "G-DJ0NLNG2LN"
 };
 
-// Initialize Firebase App
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firebase Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
